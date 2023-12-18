82d647c0b34d012a4b5f33a73e279b46fa05a523
ariguabaoli <martin_urza@hotmail.com>
2023-12-18 16:56:38 -0300

    commit

--- components/CartStatus.js
+++ components/CartStatus.js
@@ -1,41 +1,70 @@
 // CartStatus.js
 import React from "react";
 import { useCart } from "./cartcontex";
-
+import { useRouter } from "next/navigation";
 
 const CartStatus = () => {
   const { cart, removeFromCart } = useCart();
+  const router = useRouter();
 
-  const handleRemoveFromCart = (product) => {
-    removeFromCart(product);
+  const handleContinueShopping = () => {
+    router.push("/productos");
+  };
+
+  const handleFinishPurchase = () => {
+    const total = Object.values(cart).reduce((total, product) => total + product.precio * product.quantity, 0);
+    
+    // Obtener la lista de productos comprados
+    const productsPurchased = Object.values(cart).map((product) => ({
+      nombre: product.nombre,
+      cantidad: product.quantity,
+      precioUnitario: product.precio,
+      totalProducto: product.precio * product.quantity,
+    }));
+
+    // Mostrar los productos comprados en un alert
+    alert(`Total de la factura: $${total}\n\nProductos comprados:\n${JSON.stringify(productsPurchased, null, 2)}`);
   };
 
   return (
     <div>
-      <h2 className="text 2xl:">Estado del Carrito:</h2>
+      <h2 className="text-2xl">Estado del Carrito:</h2>
       {cart.length === 0 ? (
         <p className="text-xl text">El carrito está vacío.</p>
       ) : (
-        <ul>
-          {cart.map((product, index) => (
-            <li key={index}>
-              <img
-                src={product.imagen}
-                alt={product.nombre}
-                style={{ width: "50px", height: "50px", marginRight: "10px" }}
-              />
-              <span>{product.nombre}</span>
-              <span>Precio: ${product.precio}</span>
-           
-             <button className="bg-red-500"  onClick={() => handleRemoveFromCart(product)}>
-                Eliminar del carrito
-              </button>
-
-          
-            
-            </li>
-          ))}
-        </ul>
+        <>
+          <ul>
+            {Object.values(cart).map((product) => (
+              <li key={product.id} className="flex items-center mb-4">
+                <img
+                  src={product.imagen}
+                  alt={product.nombre}
+                  style={{ width: "50px", height: "50px", marginRight: "10px" }}
+                />
+                <div className="flex-grow">
+                  <span className="font-semibold">{product.nombre}</span>
+                  <span className="block text-gray-500">{product.descripcion}</span>
+                  <span className="block text-gray-500">Precio: ${product.precio}</span>
+                  <span className="block text-gray-500">Cantidad: {product.quantity}</span>
+                </div>
+                <button className="bg-red-500" onClick={() => removeFromCart(product)}>
+                  Eliminar del carrito
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-4">
+            <h3 className="text-xl font-semibold">
+              Total: ${Object.values(cart).reduce((total, product) => total + product.precio * product.quantity, 0)}
+            </h3>
+            <button className="bg-green-500 text-white px-4 py-2 rounded-md mr-4" onClick={handleContinueShopping}>
+              Comprar más
+            </button>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={handleFinishPurchase}>
+              Terminar con la compra
+            </button>
+          </div>
+        </>
       )}
     </div>
   );
--- components/cartcontex.js
+++ components/cartcontex.js
@@ -1,52 +1,72 @@
-"use client"
-//cartcontex.js
+
+
+"use client"// cartcontex.js
+// cartcontex.js
 import React, { createContext, useContext, useReducer } from "react";
 
-// Definir el contexto
 const CartContext = createContext();
 
-// Definir el inicializador del estado y el contexto del proveedor
 const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
-      return {
-        ...state,
-        cart: [...state.cart, action.payload],
-      };
-      case "REMOVE_FROM_CART":
+      const productId = action.payload.id;
+
+      if (state.cart[productId]) {
+        // El producto ya existe en el carrito, aumentar la cantidad
         return {
           ...state,
-          cart: state.cart.filter((item) => item.id !== action.payload.id),
+          cart: {
+            ...state.cart,
+            [productId]: {
+              ...state.cart[productId],
+              quantity: state.cart[productId].quantity + 1,
+            },
+          },
         };
-        
+      } else {
+        // El producto no está en el carrito, agregarlo con cantidad 1
+        return {
+          ...state,
+          cart: {
+            ...state.cart,
+            [productId]: {
+              ...action.payload,
+              quantity: 1,
+            },
+          },
+        };
+      }
 
+    case "REMOVE_FROM_CART":
+      const { [action.payload.id]: removedProduct, ...newCart } = state.cart;
+      return {
+        ...state,
+        cart: newCart,
+      };
 
-    // Agregar más casos según sea necesario (eliminar del carrito, actualizar cantidades, etc.)
     default:
       return state;
   }
 };
 
 const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(cartReducer, { cart: [] });
+  const [state, dispatch] = useReducer(cartReducer, { cart: {} });
 
   const addToCart = (product) => {
     dispatch({ type: "ADD_TO_CART", payload: product });
   };
-  
+
   const removeFromCart = (product) => {
     dispatch({ type: "REMOVE_FROM_CART", payload: product });
   };
-  // Agregar más funciones según sea necesario (eliminar del carrito, actualizar cantidades, etc.)
 
   return (
-    <CartContext.Provider value={{ ...state, addToCart, removeFromCart}}>
+    <CartContext.Provider value={{ ...state, addToCart, removeFromCart }}>
       {children}
     </CartContext.Provider>
   );
 };
 
-// Función de utilidad para usar el contexto
 const useCart = () => {
   const context = useContext(CartContext);
   if (!context) {
